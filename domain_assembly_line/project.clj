138cefd6c8a3f517e86ba36277e7be0d288e293b
Johan Jonasson <johan.jonasson@timezynk.com>
2014-11-05 14:47:57 +0100

    Correct url in project file. Dependency on clojure 1.6.0

--- domain_assembly_line/project.clj
+++ domain_assembly_line/project.clj
@@ -1,6 +1,6 @@
 (defproject com.timezynk.domain/assembly-line "0.1.0"
   :description "The assembly line of the domain project"
-  :url "https://github.com/TimeZynk/domain/domain-assembly-line"
+  :url "https://github.com/TimeZynk/domain/tree/master/domain_assembly_line"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]])
