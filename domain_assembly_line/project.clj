c7301a7a00649d321142b7bbe27d8ad64012352d
Johan Jonasson <johan.jonasson@timezynk.com>
2014-11-05 17:07:04 +0100

    Adds some stuff to the project file

--- domain_assembly_line/project.clj
+++ domain_assembly_line/project.clj
@@ -1,6 +1,8 @@
-(defproject com.timezynk.domain/assembly-line "0.1.0"
+(defproject com.timezynk.domain/assembly-line "0.2.0-SNAPSHOT"
   :description "The assembly line of the domain project"
   :url "https://github.com/TimeZynk/domain/tree/master/domain_assembly_line"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :scm {:name "git"
+        :url  "https://github.com/TimeZynk/domain"}
   :dependencies [[org.clojure/clojure "1.6.0"]])
