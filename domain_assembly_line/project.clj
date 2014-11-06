cce750c0228abbb84ea724a2d17a9fc0fa4fd945
Johan Jonasson <johan.jonasson@timezynk.com>
2014-11-06 11:20:21 +0100

    updated description

--- domain_assembly_line/project.clj
+++ domain_assembly_line/project.clj
@@ -1,5 +1,5 @@
-(defproject com.timezynk.domain/assembly-line "0.2.0-SNAPSHOT"
-  :description "The assembly line of the domain project"
+(defproject com.timezynk.domain/assembly-line "0.2.0"
+  :description "Assembly lines was initially created to model CRUD operations. An assembly line takes an input value that is processed by different stations. The stations can be changed or removed and new stations can be added."
   :url "https://github.com/TimeZynk/domain/tree/master/domain_assembly_line"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
