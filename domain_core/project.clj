388c589643874d6a83dcfe24a7eed7a9200b5d5d
Johan Jonasson <johan.jonasson@timezynk.com>
2014-11-07 10:43:38 +0100

    added scm to the project file

--- domain_core/project.clj
+++ domain_core/project.clj
@@ -1,8 +1,10 @@
 (defproject com.timezynk.domain.domain-core "0.2.0-SNAPSHOT"
   :description "Core functionality of the domain project"
-  :url "https://github.com/TimeZynk/domain/domain-core"
+  :url "https://github.com/TimeZynk/domain/tree/master/domain_core"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :scm {:name "git"
+        :url  "https://github.com/TimeZynk/domain"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.2.1"]
                  [slingshot "0.12.1"]
