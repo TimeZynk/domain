a68db373d440b2b7d804e6f1883f43bd05108626
Jonasson <j.jonasson@gmail.com>
2014-11-15 14:09:22 +0100

    Default props for domain-versioned-mongo

--- domain_example/src/domain_example/domain.clj
+++ domain_example/src/domain_example/domain.clj
@@ -4,28 +4,15 @@
             [com.timezynk.domain                     :as dom]
             [com.timezynk.domain.schema              :as s]
             [com.timezynk.domain.domain-versioned-mongo.mongo-collection
-             :refer [mongo-collection collection-name]])
+             :refer [mongo-collection collection-name]]
+            [com.timezynk.domain.domain-versioned-mongo.prop-types :as p])
   (:import [com.timezynk.domain.persistence Persistence]))
-
-;; Todo: These should be part of domain core, right?
-(def default-properties {:id         (s/id :optional? true
-                                           :remove-on-create? true)
-                         :vid        (s/id :optional? true
-                                           :remove-on-create? true)
-                         :pid        (s/id :optional? true
-                                           :remove-on-create? true)
-                         :valid-from (s/timestamp :optional? true
-                                                  :remove-on-create? true
-                                                  :remove-on-update? true)
-                         :valid-to   (s/timestamp :optional? true
-                                                  :remove-on-create? true
-                                                  :remove-on-update? true)})
 
 (defn dom-type [& {:as options}]
   (dom/dom-type (assoc options
                   :collection-factory #(mongo-collection
                                         (collection-name %)))
-                default-properties))
+                p/default-props))
 
 (defmacro defdomtype [n & opts]
   `(def ~n (dom-type ~@opts)))
--- domain_versioned_mongo/src/com/timezynk/domain/domain_versioned_mongo/prop_types.clj
+++ domain_versioned_mongo/src/com/timezynk/domain/domain_versioned_mongo/prop_types.clj
@@ -1,11 +1,11 @@
 (ns com.timezynk.domain.domain-versioned-mongo.prop-types
-  (:require [com.timezynk.domain.schema :refer [defproptype]])
+  (:require [com.timezynk.domain.schema :refer [defproptype timestamp]])
   (:import [org.bson.types ObjectId]))
 
 (defproptype mongo-id
   :validate #(isa? (class %) ObjectId)
   :invalid-msg "Not a valid mongo id"
-  :?->pack #(or (instance? ObjectId %)
+  :str->pack #(or (instance? ObjectId %)
                 (re-find #"^[\da-f]{24}$" %))
   :pack #(ObjectId. %))
 
@@ -14,3 +14,13 @@
 
 (def id mongo-id)
 (def auto-id auto-mongo-id)
+
+(def default-props {:id         (id :optional? true
+                                    :remove-on-create? true)
+                    :vid        (id :optional? true
+                                    :remove-on-create? true)
+                    :pid        (id :optional? true
+                                    :remove-on-create? true)
+                    :valid-from (timestamp :optional? true
+                                           :remove-on-create? true
+                                           :remove-on-update? true)})
