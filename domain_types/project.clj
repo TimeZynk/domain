68fd64b34084beefaeb14e0b83a6b74b354522ef
Johan Heander <johan@timezynk.com>
2021-01-29 13:23:00 +0100

    Update dependencies

--- domain_types/project.clj
+++ domain_types/project.clj
@@ -1,11 +1,11 @@
-(defproject com.timezynk/domain-types "1.0.0"
+(defproject com.timezynk/domain-types "1.0.1"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "BSD 3 Clause"
             :url "https://opensource.org/licenses/BSD-3-Clause"}
-  :dependencies [[com.timezynk/domain "1.0.0"]
-                 [com.timezynk/useful "1.8.0" :scope "dev"]
-                 [congomongo "2.1.0" :scope "provided"]
+  :dependencies [[com.timezynk/domain "1.1.0"]
+                 [com.timezynk/useful "1.10.1" :scope "dev"]
+                 [congomongo "2.2.1" :scope "provided"]
                  [org.clojure/clojure "1.10.0" :scope "provided"]
                  [slingshot "0.12.2"]]
   :repl-options {:init-ns domain-types.core}
