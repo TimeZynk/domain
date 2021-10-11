15e7f4899285201705a4ba3949e9bc8369796953
Lars <lars.melander@gmail.com>
2021-10-11 10:40:03 +0200

    Fixing structure

--- domain/src/com/timezynk/domain/validation/only_these.clj
+++ domain/src/com/timezynk/domain/validation/only_these.clj
@@ -1,6 +1,7 @@
 (ns com.timezynk.domain.validation.only-these
   (:require
-   [clojure.set :refer [difference]]))
+   [clojure.set :refer [difference]]
+   [clojure.test :refer [deftest is]]))
 
 (defn only-these
   "only these are valid attributes"
@@ -13,3 +14,11 @@
       (if (empty? invalid-attrs)
         [true {}]
         [false (apply merge (map (fn [attr] {attr "invalid attribute"}) invalid-attrs))]))))
+
+(deftest test-only-these
+  (is (= [true {}]
+         ((only-these :field1 :field2) {:field1 ""
+                                        :field2 ""})))
+  (is (= [false {:field3 "invalid attribute"}]
+         ((only-these :field1 :field2) {:field1 ""
+                                        :field3 ""}))))
---
+++ domain/test/com/timezynk/domain/validation_test.clj
@@ -0,0 +1,13 @@
+(ns com.timezynk.domain.validation-test
+  (:require
+   [clojure.test :refer [deftest is testing]]
+   [com.timezynk.domain.validation :as v])
+  (:import [org.joda.time LocalDate]))
+
+(deftest function-calls
+  (testing "Functions are called correctly"
+    (let [func (v/all-of (v/lt= :start :end)
+                         (v/eq :start :end))]
+      (is (= [true {}]
+             (func {:start (LocalDate. "2010-01-02")
+                    :end (LocalDate. "2010-01-02")}))))))
