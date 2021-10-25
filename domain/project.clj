dbc7b91ecf81872f8bd41050fef98a856eeecceb
Lars <lars.melander@gmail.com>
2021-10-25 14:10:01 +0200

    Version update reflecting bugfix.

--- domain/project.clj
+++ domain/project.clj
@@ -1,4 +1,4 @@
-(defproject com.timezynk/domain "1.3.0"
+(defproject com.timezynk/domain "1.3.1"
   :description "Database modeling library for Clojure and MongoDB"
   :url "https://github.com/TimeZynk/domain/tree/master/domain"
   :license {:name "BSD 3 Clause"
