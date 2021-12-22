706639f5d48361825ca2c3a4e9269e2e97b70ddd
eureton <48595045+eureton@users.noreply.github.com>
2021-12-22 15:39:57 +0200

    Fix: mitigate log4j vulnerability (#8)
    * domain: Apply mitigation
    
    * Source patched tz libs
    
    * Bump version numbers
    
    * Specify log factory for all profiles
    
    Co-authored-by: Agis Theodoropoulos <agis@eureton.com>

--- domain/project.clj
+++ domain/project.clj
@@ -1,4 +1,4 @@
-(defproject com.timezynk/domain "1.4.1"
+(defproject com.timezynk/domain "1.4.2"
   :description "Database modeling library for Clojure and MongoDB"
   :url "https://github.com/TimeZynk/domain/tree/master/domain"
   :license {:name "BSD 3 Clause"
@@ -7,11 +7,12 @@
         :url  "https://github.com/TimeZynk/domain"}
   :dependencies [[com.novemberain/validateur "1.2.0"]
                  [com.timezynk/assembly-line "1.0.0"]
-                 [com.timezynk/useful "1.9.0"]
+                 [com.timezynk/useful "1.13.4"]
                  [compojure "1.5.1" :scope "provided"]
                  [congomongo "2.1.0" :scope "provided"]
                  [org.clojure/clojure "1.10.1" :scope "provided"]
-                 [org.clojure/tools.logging "1.1.0"]
+                 [ch.qos.logback/logback-classic "1.2.9"]
+                 [org.clojure/tools.logging "1.2.3"]
                  [slingshot "0.12.2"]
                  [tortue/spy "2.4.0"]]
   :repl-options {:init-ns com.timezynk.domain.core}
@@ -20,4 +21,5 @@
   :profiles {:kaocha {:dependencies [[lambdaisland/kaocha "1.0.632"]]
                       :jvm-opts ["-Djdk.tls.client.protocols=TLSv1,TLSv1.1,TLSv1.2"]}}
 
+  :jvm-opts ["-Dclojure.tools.logging.factory=clojure.tools.logging.impl/slf4j-factory"]
   :aliases {"kaocha" ["with-profile" "+kaocha" "run" "-m" "kaocha.runner"]})
