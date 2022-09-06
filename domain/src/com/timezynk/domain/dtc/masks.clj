a69e087d9ab99cc1f5bfbd3fbe782ed504ef4b2f
Agis Theodoropoulos <agis@eureton.com>
2022-09-06 18:29:25 +0300

    Use helper to hide cancan implementation details

--- domain/src/com/timezynk/domain/dtc/masks.clj
+++ domain/src/com/timezynk/domain/dtc/masks.clj
@@ -1,16 +1,16 @@
 (ns com.timezynk.domain.dtc.masks
   "Useful functions for masking DTC properties."
-  (:require [com.timezynk.useful.cancan :refer [can? *ability*]]
+  (:require [com.timezynk.useful.cancan :as ability]
             [com.timezynk.domain.core :as c]))
 
 (defn unauthorized?
   "Truthy if doc is missing authorization to contain the incoming property.
-   Truthy if the cancan/*ability* object is unbound.
+   Truthy if not called within a cancan scope.
    Falsy, otherwise."
   [dtc doc action property-name]
-  (when (bound? #'*ability*)
+  (when (ability/in-scope?)
     (let [action (keyword (format "%s-property-%s"
                                   (name action)
                                   (name property-name)))
           object (c/get-dtc-name dtc)]
-      (not (can? action object doc)))))
+      (not (ability/can? action object doc)))))
