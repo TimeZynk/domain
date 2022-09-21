9791c91b2848114a869d3c815f784c9f7f4a5fd4
Agis Theodoropoulos <agis@eureton.com>
2022-09-21 19:13:28 +0300

    DTC variables don't need to be dynamic

--- domain/test/com/timezynk/domain/schema/vector_test.clj
+++ domain/test/com/timezynk/domain/schema/vector_test.clj
@@ -1,5 +1,5 @@
 (ns com.timezynk.domain.schema.vector-test
-  (:require [clojure.test :refer [deftest is testing use-fixtures]]
+  (:require [clojure.test :refer [deftest is testing]]
             [com.timezynk.assembly-line :as a]
             [com.timezynk.domain.core :as c]
             [com.timezynk.domain.persistence :as p]
@@ -7,23 +7,18 @@
             [slingshot.test])
   (:import [org.bson.types ObjectId]))
 
-(def ^:private ^:dynamic *dtc*)
-
-(defn- create-dtc [f]
-  (binding [*dtc* (c/dom-type-collection :name :qwerty
-                                         :properties {:x (s/vector (s/integer))})]
-    (f)))
+(def ^:private dtc
+  (c/dom-type-collection :name :qwerty
+                         :properties {:x (s/vector (s/integer))}))
 
 (def ^:private ^:const valid-doc
   {:x [1 2 3]
    :company-id (ObjectId.)})
 
 (defn- insert! [doc]
-  (-> (p/conj! *dtc* doc)
+  (-> (p/conj! dtc doc)
       (a/add-stations :replace :execute [:nop (fn [_ doc] doc)])
       deref))
-
-(use-fixtures :each create-dtc)
 
 (deftest valid
   (is (insert! valid-doc)))
