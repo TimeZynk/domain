9bbc361cb76b1dd091eee0602765132070802471
Agis Theodoropoulos <agis@eureton.com>
2022-09-21 10:23:36 +0300

    Fix broken tests

--- domain/test/com/timezynk/domain/dtc/masks_test.clj
+++ domain/test/com/timezynk/domain/dtc/masks_test.clj
@@ -1,6 +1,6 @@
 (ns com.timezynk.domain.dtc.masks-test
   (:require [clojure.test :refer [deftest is testing compose-fixtures use-fixtures]]
-            [spy.core :refer [stub spy called-once? call-matching?]]
+            [spy.core :refer [stub spy called-n-times? call-matching?]]
             [com.timezynk.domain.core :as c]
             [com.timezynk.domain.dtc.masks :refer [unauthorized?]]
             [com.timezynk.domain.schema :as s]
@@ -15,10 +15,16 @@
 (def ^:const dtc-properties
   {:x (s/string)
    :y (s/string :mask unauthorized?)
-   :z (s/string)})
+   :z (s/map {:z1 (s/string)
+              :z2 (s/string :mask unauthorized?)
+              :z3 (s/string)})})
 
 (def ^:const records
-  [{:x "abc" :y "123" :z "xyz"}])
+  [{:x "abc"
+    :y "123"
+    :z {:z1 "xyz-1"
+        :z2 "xyz-2"
+        :z3 "xyz-3"}}])
 
 (defn- create-dtc [f]
   (binding [*dtc* (c/dom-type-collection :name :qwerty
@@ -43,32 +49,71 @@
       (testing "unmarked properties"
         (is (contains? doc :x))
         (is (contains? doc :z)))
+      (testing "marked nested property"
+        (is (not (contains? (:z doc) :z2))))
+      (testing "unmarked nested properties"
+        (is (contains? (:z doc) :z1))
+        (is (contains? (:z doc) :z3)))
       (testing "authorization"
         (testing "times called"
-          (is (called-once? ability/can?)))
-        (testing "action"
+          (is (called-n-times? ability/can? 2)))
+        (testing "top-level property action"
           (is (call-matching? ability/can? (comp #{:read-property-y} first))))
+        (testing "nested property action"
+          (is (call-matching? ability/can? (comp #{:read-property-z.z2} first))))
         (testing "object"
           (is (call-matching? ability/can? (comp #{:qwerty} second))))))))
 
+(defn- mask-matcher
+  "Builds a predicate suitable for spy.core/call-matching? which checks whether
+   a (potentially nested) property is present in the incoming document."
+  [masked? property-path]
+  (let [prefix (pop property-path)
+        property (peek property-path)]
+    (fn [doc]
+      (cond-> doc
+        (not-empty prefix) (get-in prefix)
+        true (contains? property)
+        masked? (not)))))
+
 (deftest creating
-  (testing "Adding to the store"
-    @(p/conj! *dtc* {:x "cba" :y "321" :z "zyx" :company-id (ObjectId.)})
-    (testing "marked property"
-      (is (call-matching? m/insert! (fn [[_ new-doc]]
-                                      (let [new-doc (peek (into [] new-doc))]
-                                        (not (contains? new-doc :y)))))))
-    (testing "unmarked property"
-      (is (call-matching? m/insert! (fn [[_ new-doc]]
-                                      (let [new-doc (peek (into [] new-doc))]
-                                        (contains? new-doc :z))))))))
+  (let [args->doc #(->> % last (into []) (peek))
+        masked #(comp (mask-matcher true %) args->doc)
+        not-masked #(comp (mask-matcher false %) args->doc)]
+    (testing "Adding to the store"
+      @(p/conj! *dtc* {:x "cba"
+                       :y "321"
+                       :z {:z1 "zyx-1"
+                           :z2 "zyx-2"
+                           :z3 "zyx-3"}
+                       :company-id (ObjectId.)})
+      (testing "marked property"
+        (is (call-matching? m/insert! (masked [:y]))))
+      (testing "unmarked properties"
+        (is (call-matching? m/insert! (not-masked [:x])))
+        (is (call-matching? m/insert! (not-masked [:z]))))
+      (testing "marked nested property"
+        (is (call-matching? m/insert! (masked [:z :z2]))))
+      (testing "unmarked nested properties"
+        (is (call-matching? m/insert! (not-masked [:z :z1])))
+        (is (call-matching? m/insert! (not-masked [:z :z3])))))))
 
 (deftest updating
-  (testing "Updating the store"
-    @(p/update-in! *dtc* {} {:y "321" :z "zyx"})
-    (testing "marked property"
-      (is (call-matching? m/update! (fn [[_ _ new-doc]]
-                                      (not (contains? new-doc :y))))))
-    (testing "unmarked property"
-      (is (call-matching? m/update! (fn [[_ _ new-doc]]
-                                      (contains? new-doc :z)))))))
+  (let [masked #(comp (mask-matcher true %) last)
+        not-masked #(comp (mask-matcher false %) last)]
+    (testing "Updating the store"
+      @(p/update-in! *dtc* {} {:x "cba"
+                               :y "321"
+                               :z {:z1 "zyx-1"
+                                   :z2 "zyx-2"
+                                   :z3 "zyx-3"}})
+      (testing "marked property"
+        (is (call-matching? m/update! (masked [:y]))))
+      (testing "unmarked properties"
+        (is (call-matching? m/update! (not-masked [:x])))
+        (is (call-matching? m/update! (not-masked [:z]))))
+      (testing "marked nested property"
+        (is (call-matching? m/update! (masked [:z :z2]))))
+      (testing "unmarked nested properties"
+        (is (call-matching? m/update! (not-masked [:z :z1])))
+        (is (call-matching? m/update! (not-masked [:z :z3])))))))
