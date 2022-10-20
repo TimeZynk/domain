b2ed88813895c1ad0dc46d3bfb2afd1cfe2d9228
Agis Theodoropoulos <agis@eureton.com>
2022-10-20 14:09:22 +0300

    Bump patch version

--- domain/project.clj
+++ domain/project.clj
@@ -1,4 +1,4 @@
-(defproject com.timezynk/domain "1.6.4"
+(defproject com.timezynk/domain "1.6.5"
   :description "Database modeling library for Clojure and MongoDB"
   :url "https://github.com/TimeZynk/domain/tree/master/domain"
   :license {:name "BSD 3 Clause"
