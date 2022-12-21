b3932845f39ad0ccde73c6e02bb50fb238f1778b
Lars <lars.melander@gmail.com>
2022-12-21 09:15:17 +0100

    Version.

--- domain/project.clj
+++ domain/project.clj
@@ -1,4 +1,4 @@
-(defproject com.timezynk/domain "1.6.6"
+(defproject com.timezynk/domain "1.6.7"
   :description "Database modeling library for Clojure and MongoDB"
   :url "https://github.com/TimeZynk/domain/tree/master/domain"
   :license {:name "BSD 3 Clause"
