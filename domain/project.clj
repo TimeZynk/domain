958d5b204377b2136f61c5b96d867b5b8300cde2
Lars <lars.melander@gmail.com>
2023-03-23 12:58:13 +0100

    Dependencies update.

--- domain/project.clj
+++ domain/project.clj
@@ -1,20 +1,20 @@
-(defproject com.timezynk/domain "1.6.9"
+(defproject com.timezynk/domain "1.6.10"
   :description "Database modeling library for Clojure and MongoDB"
   :url "https://github.com/TimeZynk/domain/tree/master/domain"
   :license {:name "BSD 3 Clause"
             :url "https://opensource.org/licenses/BSD-3-Clause"}
   :scm {:name "git"
         :url  "https://github.com/TimeZynk/domain"}
-  :dependencies [[com.novemberain/validateur "1.2.0"]
+  :dependencies [[ch.qos.logback/logback-classic "1.4.6"]
+                 [com.novemberain/validateur "2.6.0"]
                  [com.timezynk/assembly-line "1.0.0"]
-                 [com.timezynk/useful "1.20.3"]
-                 [compojure "1.5.1" :scope "provided"]
-                 [congomongo "2.1.0" :scope "provided"]
-                 [org.clojure/clojure "1.10.1" :scope "provided"]
-                 [ch.qos.logback/logback-classic "1.2.9"]
-                 [org.clojure/tools.logging "1.2.3"]
+                 [com.timezynk/useful "1.20.13"]
+                 [compojure "1.7.0" :scope "provided" :exclusions [commons-codec]]
+                 [congomongo "2.6.0" :scope "provided"]
+                 [org.clojure/clojure "1.11.1" :scope "provided"]
+                 [org.clojure/tools.logging "1.2.4"]
                  [slingshot "0.12.2"]
-                 [tortue/spy "2.4.0"]]
+                 [tortue/spy "2.13.0"]]
   :repl-options {:init-ns com.timezynk.domain.core}
   :test-paths ["src" "test"]
   :plugins [[lein-cljfmt "0.6.7"]]
