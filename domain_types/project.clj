3e77cf930e618cb5742f88cb24d5bf92642cea7a
Agis Theodoropoulos <agis@eureton.com>
2023-04-12 10:53:59 +0300

    domain_types: Upgrade clojure and useful

--- domain_types/project.clj
+++ domain_types/project.clj
@@ -4,9 +4,9 @@
   :license {:name "BSD 3 Clause"
             :url "https://opensource.org/licenses/BSD-3-Clause"}
   :dependencies [[com.timezynk/domain "2.0.0"]
-                 [com.timezynk/useful "1.13.4" :scope "dev"]
+                 [com.timezynk/useful "1.21.0" :scope "dev"]
                  [congomongo "2.2.1" :scope "provided"]
-                 [org.clojure/clojure "1.10.0" :scope "provided"]
+                 [org.clojure/clojure "1.11.1" :scope "provided"]
                  [slingshot "0.12.2"]]
   :repl-options {:init-ns domain-types.core}
   :plugins [[lein-cljfmt "0.6.7"]])
