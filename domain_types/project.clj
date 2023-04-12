a7f2e8381f25b1fc72adb7778a7c1a895fe1dc5d
Agis Theodoropoulos <agis@eureton.com>
2023-04-12 22:18:38 +0300

    domain_types: Update dependencies

--- domain_types/project.clj
+++ domain_types/project.clj
@@ -3,10 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "BSD 3 Clause"
             :url "https://opensource.org/licenses/BSD-3-Clause"}
-  :dependencies [[com.timezynk/domain "1.1.0"]
-                 [com.timezynk/useful "1.13.4" :scope "dev"]
-                 [congomongo "2.2.1" :scope "provided"]
-                 [org.clojure/clojure "1.10.0" :scope "provided"]
+  :dependencies [[com.timezynk/domain "1.7.0"]
+                 [com.timezynk/useful "2.0.0" :scope "dev"]
+                 [congomongo "2.6.0" :scope "provided"]
+                 [org.clojure/clojure "1.11.1" :scope "provided"]
                  [slingshot "0.12.2"]]
   :repl-options {:init-ns domain-types.core}
   :plugins [[lein-cljfmt "0.6.7"]])
