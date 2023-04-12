e7a616ebfdf1922efc41890b9faf818074eb4178
Agis Theodoropoulos <agis@eureton.com>
2023-04-12 22:03:15 +0300

    Remove unused dependency
    The domain library logs nothing.

--- domain/project.clj
+++ domain/project.clj
@@ -5,14 +5,12 @@
             :url "https://opensource.org/licenses/BSD-3-Clause"}
   :scm {:name "git"
         :url  "https://github.com/TimeZynk/domain"}
-  :dependencies [[ch.qos.logback/logback-classic "1.4.6"]
-                 [com.novemberain/validateur "2.6.0"]
+  :dependencies [[com.novemberain/validateur "2.6.0"]
                  [com.timezynk/assembly-line "1.0.0"]
                  [com.timezynk/useful "1.20.13"]
                  [compojure "1.7.0" :scope "provided" :exclusions [commons-codec]]
                  [congomongo "2.6.0" :scope "provided"]
                  [org.clojure/clojure "1.11.1" :scope "provided"]
-                 [org.clojure/tools.logging "1.2.4"]
                  [slingshot "0.12.2"]
                  [tortue/spy "2.13.0"]]
   :repl-options {:init-ns com.timezynk.domain.core}
@@ -20,6 +18,4 @@
   :plugins [[lein-cljfmt "0.6.7"]]
   :profiles {:kaocha {:dependencies [[lambdaisland/kaocha "1.0.632"]]
                       :jvm-opts ["-Djdk.tls.client.protocols=TLSv1,TLSv1.1,TLSv1.2"]}}
-
-  :jvm-opts ["-Dclojure.tools.logging.factory=clojure.tools.logging.impl/slf4j-factory"]
   :aliases {"kaocha" ["with-profile" "+kaocha" "run" "-m" "kaocha.runner"]})
