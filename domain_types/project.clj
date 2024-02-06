6004dcd9aac1da76c21dcf2b4d4148a27b9c4e51
eureton <48595045+eureton@users.noreply.github.com>
2024-02-06 10:37:07 +0200

    Feature: Source latest version of useful library (#25)
    * Source latest version
    
    * Bump patch version
    
    * Source latest versions
    
    * Bump patch version

--- domain_types/project.clj
+++ domain_types/project.clj
@@ -1,10 +1,10 @@
-(defproject com.timezynk/domain-types "1.0.7"
+(defproject com.timezynk/domain-types "1.0.8"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "BSD 3 Clause"
             :url "https://opensource.org/licenses/BSD-3-Clause"}
-  :dependencies [[com.timezynk/domain "2.2.1"]
-                 [com.timezynk/useful "2.5.0" :scope "dev"]
+  :dependencies [[com.timezynk/domain "2.2.2"]
+                 [com.timezynk/useful "2.6.0" :scope "dev"]
                  [congomongo "2.6.0" :scope "provided"]
                  [org.clojure/clojure "1.11.1" :scope "provided"]
                  [slingshot "0.12.2"]]
