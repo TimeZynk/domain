973e36d26b4582d1401b5b44512fb94dc05d2d1e
eureton <48595045+eureton@users.noreply.github.com>
2024-03-13 14:02:46 +0200

    Bugfix: Update dependencies (#27)
    * Source latest useful lib
    
    * Bump patch version
    
    * Source latest useful, domain libs
    
    * Bump patch version
    
    * Source latest cancancan lib

--- domain_types/project.clj
+++ domain_types/project.clj
@@ -1,10 +1,10 @@
-(defproject com.timezynk/domain-types "1.0.10"
+(defproject com.timezynk/domain-types "1.0.11"
   :description "Modeling extras built on top of domain"
   :url "https://github.com/TimeZynk/domain/tree/master/domain_types"
   :license {:name "BSD 3 Clause"
             :url "https://opensource.org/licenses/BSD-3-Clause"}
-  :dependencies [[com.timezynk/domain "2.2.4"]
-                 [com.timezynk/useful "2.6.4" :scope "dev"]
+  :dependencies [[com.timezynk/domain "2.2.5"]
+                 [com.timezynk/useful "2.6.5" :scope "dev"]
                  [congomongo "2.6.0" :scope "provided"]
                  [org.clojure/clojure "1.11.1" :scope "provided"]
                  [slingshot "0.12.2"]]
