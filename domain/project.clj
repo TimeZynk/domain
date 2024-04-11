ffe2adb79ecb88acbd471db953ffcf011f90b0de
eureton <48595045+eureton@users.noreply.github.com>
2024-04-11 15:44:32 +0300

    Fix: Throttle mchan threads (#30)
    * Source throttling fix
    
    * Bump patch version
    
    * Source throttling fix
    
    * Get latest domain version
    
    * Bump patch version
    
    * Source throttle tweaks
    
    * Source throttle tweaks
    
    * Source threshold fix
    
    * Source threshold fix

--- domain/project.clj
+++ domain/project.clj
@@ -1,4 +1,4 @@
-(defproject com.timezynk/domain "2.2.7"
+(defproject com.timezynk/domain "2.2.8"
   :description "Database modeling library for Clojure and MongoDB"
   :url "https://github.com/TimeZynk/domain/tree/master/domain"
   :license {:name "BSD 3 Clause"
@@ -8,7 +8,7 @@
   :dependencies [[com.novemberain/validateur "1.2.0"]
                  [com.timezynk/assembly-line "1.0.1"]
                  [com.timezynk/cancancan "0.3.0"]
-                 [com.timezynk/useful "2.7.0"]
+                 [com.timezynk/useful "2.7.3"]
                  [compojure "1.7.0" :scope "provided" :exclusions [commons-codec]]
                  [congomongo "2.6.0" :scope "provided"]
                  [org.clojure/clojure "1.11.1" :scope "provided"]
