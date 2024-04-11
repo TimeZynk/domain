cf004ad8b88fd185b5c7041c346f6dc01f6f2cb5
Agis Theodoropoulos <agis@eureton.com>
2024-04-11 10:51:46 +0300

    Source throttling fix

--- domain_types/project.clj
+++ domain_types/project.clj
@@ -4,7 +4,7 @@
   :license {:name "BSD 3 Clause"
             :url "https://opensource.org/licenses/BSD-3-Clause"}
   :dependencies [[com.timezynk/domain "2.2.7"]
-                 [com.timezynk/useful "2.7.0" :scope "dev"]
+                 [com.timezynk/useful "2.7.1" :scope "dev"]
                  [congomongo "2.6.0" :scope "provided"]
                  [org.clojure/clojure "1.11.1" :scope "provided"]
                  [slingshot "0.12.2"]]
