5e5fe74b90d581a3515b9992f0527b35e20bff98
Agis Theodoropoulos <agis@eureton.com>
2024-04-27 23:26:30 +0300

    Update dependencies

--- domain_types/project.clj
+++ domain_types/project.clj
@@ -3,8 +3,8 @@
   :url "https://github.com/TimeZynk/domain/tree/master/domain_types"
   :license {:name "BSD 3 Clause"
             :url "https://opensource.org/licenses/BSD-3-Clause"}
-  :dependencies [[com.timezynk/domain "2.2.8"]
-                 [com.timezynk/useful "2.7.3" :scope "dev"]
+  :dependencies [[com.timezynk/domain "2.3.0"]
+                 [com.timezynk/useful "4.2.0" :scope "dev"]
                  [congomongo "2.6.0" :scope "provided"]
                  [org.clojure/clojure "1.11.1" :scope "provided"]
                  [slingshot "0.12.2"]]
