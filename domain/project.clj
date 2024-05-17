fb9caa8a1ff0793d92bb650dd7b0a3545587c684
Agis Theodoropoulos <agis@eureton.com>
2024-05-17 18:38:48 +0300

    Source latest version of bus library

--- domain/project.clj
+++ domain/project.clj
@@ -7,7 +7,7 @@
         :url  "https://github.com/TimeZynk/domain"}
   :dependencies [[com.novemberain/validateur "1.2.0"]
                  [com.timezynk/assembly-line "1.0.1"]
-                 [com.timezynk/bus "1.2.0"]
+                 [com.timezynk/bus "1.2.2"]
                  [com.timezynk/cancancan "0.3.0"]
                  [com.timezynk/useful "4.5.0"]
                  [compojure "1.7.0" :scope "provided" :exclusions [commons-codec]]
