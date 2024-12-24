646acfeb1d50fcfb7c34ea8cf764e547585f28e5
Agis Theodoropoulos <agis@eureton.com>
2024-12-24 22:27:15 +0200

    Bump patch version

--- domain_types/project.clj
+++ domain_types/project.clj
@@ -1,4 +1,4 @@
-(defproject com.timezynk/domain-types "1.0.19"
+(defproject com.timezynk/domain-types "1.0.20"
   :description "Modeling extras built on top of domain"
   :url "https://github.com/TimeZynk/domain/tree/master/domain_types"
   :license {:name "BSD 3 Clause"
